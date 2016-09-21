3d549bf534804c1d2482cc6f4f4180477403f579
Maksim Zhylinski <uzzable@gmail.com>
2016-09-21 12:45:26 +0300

    Add "logs" and "metrics" links

--- app/ui/app/common/target-provider/TargetSelectorController.js
+++ app/ui/app/common/target-provider/TargetSelectorController.js
@@ -16,10 +16,11 @@
 (function () {
     "use strict";
 
-    App.controller('TargetSelectorController', function (targetProvider, $scope, UserProvider, $state, $rootScope) {
+    App.controller('TargetSelectorController', function (targetProvider, $scope, UserProvider, $state, $rootScope, PlatformContextProvider) {
         var user = {};
 
         $scope.targetHeader = $state.current.targetHeader;
+        $scope.platformInfo = {};
 
         $scope.organization = {
             selected: targetProvider.getOrganization(),
@@ -73,6 +74,59 @@
                 }
             });
         });
+
+        $scope.openLogs = function() {
+            var visualizations = $scope.platformInfo.external_tools.visualizations,
+                logsItem       = _.find(visualizations, function(item) { return item.name === "logs"; } );
+                org            = targetProvider.getOrganization().name,
+                space          = targetProvider.getSpace().name,
+                url            = logsItem.url + "/app/kibana#/discover?_g=" +
+                        "(" +
+                            "refreshInterval:(display:Off,pause:!f,value:0)," +
+                            "time:(from:now-15m,mode:quick,to:now)" +
+                        ")" +
+                        "&_a=" +
+                        "(" +
+                            "columns:!(_source)," +
+                            "filters:!(" +
+                                "(" +
+                                    "'$state':(store:appState)," +
+                                    "meta:(alias:!n,disabled:!f,index:'logs-app-*',key:'@cf.org',negate:!f,value:" + org + ")," +
+                                    "query:(" +
+                                        "match:('@cf.org':(query:" + org + ",type:phrase))" +
+                                    ")" +
+                                ")," +
+                                "(" +
+                                    "'$state':(store:appState)," +
+                                    "meta:(alias:!n,disabled:!f,index:'logs-app-*',key:'@cf.space',negate:!f,value:" + space + ")," +
+                                    "query:(" +
+                                        "match:('@cf.space':(query:" + space + ",type:phrase))" +
+                                    ")" +
+                                ")" +
+                            ")," +
+                            "index:'logs-app-*'," +
+                            "interval:auto," +
+                            "query:(query_string:(analyze_wildcard:!t,query:'*'))," +
+                            "sort:!('@timestamp',desc)" +
+                        ")"
+            window.open(url);
+        };
+
+        $scope.openMetrics = function() {
+            var visualizations = $scope.platformInfo.external_tools.visualizations,
+                metricsItem    = _.find(visualizations, function(item) { return item.name === "space-metrics"; } );
+                org            = targetProvider.getOrganization().name,
+                space          = targetProvider.getSpace().name,
+                url            = metricsItem.url;
+            url = url.replace("<org>", org);
+            url = url.replace("<space>", space);
+            window.open(url);
+        };
+
+        PlatformContextProvider.getPlatformContext()
+            .then(function (response) {
+                $scope.platformInfo = response;
+            });
     });
 
     function sortOrgAndSpacesByName(items) {
